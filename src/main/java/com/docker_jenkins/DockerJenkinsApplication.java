b71710227408aeb18102b83ab46aaa336b81f450
omar <omar1807@stud.kea.dk>
2022-01-20 17:54:31 +0100

    1st commit

---
+++ src/main/java/com/docker_jenkins/DockerJenkinsApplication.java
@@ -0,0 +1,26 @@
+package com.docker_jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class DockerJenkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerJenkinsApplication.class, args);
+    }
+
+    @RestController
+    static class BootLoader{
+
+        @GetMapping("/{name}")
+        public ResponseEntity<String> askQuestion(@PathVariable String name){
+            return ResponseEntity.ok("Hey " + name + " Welcome to CI-CD pipeline, btw do you like pudding?");
+        }
+    }
+
+}
